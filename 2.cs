4b3f408839268240c682be04195119bb27fc53ee
YaroslawGarbarenko <72256923+YaroslawGarbarenko@users.noreply.github.com>
2020-11-25 09:32:16 +0200

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace lb4task3 
+{
+    public class Student
+    {
+        protected string name; 
+        protected int course; 
+        protected string gradeBook;
+
+        public Student(string Name, int course, string gradeBook)
+        {
+            this.Name = Name;
+            this.course = course;
+            this.gradeBook = gradeBook;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        public string GradeBook
+        {
+            get { return gradeBook; }
+            set { gradeBook = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("The values of fields are:");
+            Console.WriteLine($"Name = {name}");
+            Console.WriteLine($"Course = {course}");
+            Console.WriteLine($"GradeBook = {gradeBook}");
+        }
+    }
+
+    public class Aspirant : Student
+    {
+        protected string topic; 
+
+        public Aspirant(string name, int course, string gradeBook, string topic) :
+        base(name, course, gradeBook)
+        {
+            base.name = name; 
+            this.course = course; 
+            this.gradeBook = gradeBook;
+            this.topic = topic; 
+        }
+
+        public string Topic
+        {
+            get { return topic; }
+            set { topic = value; }
+        }
+
+        public new void Print() 
+        {
+            base.Print(); 
+            Console.WriteLine($"Topic = {topic}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student("Garbarenko.Y.O", 2, "34/19-304");
+            Console.WriteLine("The instance of st1:");
+            st1.Print();
+            Aspirant asp1 = new Aspirant(st1.Name, st1.Course, st1.GradeBook, "Hello world!");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("The instance of asp1:");
+            asp1.Print();
+        }
+    }
+}
---
+++ 4.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace lb4task4
+{
+    public class ApplicationLicense
+    {
+        protected string AllowTrial;
+        protected string AllowCommon;
+        protected string AllowPro;
+        public ApplicationLicense(string AllowTrial, string AllowCommon, string AllowPro)
+        {
+            this.allowTrial = AllowTrial;
+            this.allowCommon = AllowCommon;
+            this.allowPro = AllowPro;
+        }
+        public string allowTrial
+        {
+            get { return allowTrial; }
+            set { AllowTrial = value; }
+        }
+        public string allowCommon
+        {
+            get { return allowCommon; }
+            set { AllowCommon = value; }
+        }
+        public string allowPro
+        {
+            get { return allowPro; }
+            set { AllowPro = value; }
+        }
+        public void Print()
+        {
+            Console.WriteLine("The values of fields are:");
+            Console.WriteLine($"AllowTrial = {AllowTrial}");
+            Console.WriteLine($"AllowCommon = {AllowCommon}");
+            Console.WriteLine($"AllowPro= {AllowPro}");
+        }
+    }
+    public class License : ApplicationLicense
+    {
+        protected string topic;
+
+        public License(string allowTrial, string allowCommon, string allowPro, string topic) :
+        base(allowTrial, allowCommon, allowPro)
+        {
+            base.allowTrial = AllowTrial;
+            this.allowCommon = AllowCommon;
+            this.allowPro = AllowPro;
+            this.topic = topic;
+        }
+
+        public string Topic
+        {
+            get { return topic; }
+            set { topic = value; }
+        }
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Topic = {topic}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationLicense st1 = new ApplicationLicense("Триальный режим", "Бесплатная версия", "Платная версия");
+
+            Console.WriteLine("The instance of st1:");
+            st1.Print();
+            License lic1 = new License(st1.allowTrial, st1.allowCommon, st1.allowPro, "Выберите лицензию :");
+            Console.WriteLine("The instance of lic1:");
+            lic1.Print();
+        }
+    }
+}
